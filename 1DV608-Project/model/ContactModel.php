e4a508202165bd3f85ea253eee0700de570847b7
rebeccafransson <rf222cz@student.lnu.se>
2015-10-21 14:17:27 +0200

    mailTo :leaves:

--- 1DV608-Project/model/ContactModel.php
+++ 1DV608-Project/model/ContactModel.php
@@ -3,8 +3,19 @@ namespace model;
 
 class ContactModel{
   private $emailTo = 'ymafransson@gmail.com';
+  private $emailFrom;
+  private $subject = 'A message from websurfer on your site';
+  private $message;
+  private $headers;
 
   public function sendNewEmail($emailObj){
+    $this->emailFrom = $emailObj->getEmailAdress();
+    $this->message = $emailObj->getClientMessage();
+    $this->headers = 'From: '.$this->emailFrom;
+
+    mail($this->emailTo, $this->subject, $this->message, $this->headers);
+
+
     echo "nu sänder vi emailet allt gick bra ";
 
   }
